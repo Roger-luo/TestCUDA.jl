3df2a9efe9d1a2510e3655271f9fba834e463dc9
Roger-luo <hiroger@qq.com>
2018-11-26 16:40:18 -0500

    use build file

--- src/TestCUDA.jl
+++ src/TestCUDA.jl
@@ -1,7 +1,11 @@
 module TestCUDA
 
-using Requires
+const ext = joinpath(dirname(@__DIR__), "deps", "ext.jl")
+isfile(ext) || error("Batched.jl has not been built, please run Pkg.build(\"Batched\").")
+include(ext)
 
-@init @require CuArrays="3a865a2d-5b23-5a0f-bc46-62713ec82fae" include("cuda/cuda.jl")
+# using Requires
+#
+# @init @require CuArrays="3a865a2d-5b23-5a0f-bc46-62713ec82fae" include("cuda/cuda.jl")
 
 end # module
